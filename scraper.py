c9eb827cc336c2aa67d49ccb125aa27a3c1e2b46
Gabriel <rochon.gabriel1@gmail.com>
2020-06-28 15:11:43 -0400

    Added increments to votes/reads/parts

--- scraper.py
+++ scraper.py
@@ -162,19 +162,20 @@ for i in range(3):
             ws.cell(row+1, column, chap[3])
             column += 2
         column = 1
-    # else:
-    #     row = ws.max_row+1
-    #     column = 1
-    #     for chap in chap_stats_list:
-    #         yesterday = ws.cell(row=row-1, column=column).value
-    #         chap_inc = chap[i] - int(yesterday)
-    #         column += 2
-
-    # print(chap_inc)
 
     row = ws.max_row + 1
     for chap in chap_stats_list:
         ws.cell(row, column, chap[i])
         column += 2
+
+    # Increments
+    column = 2
+    if existing_file:
+        for chap in chap_stats_list:
+            yesterday = ws.cell(row=row-1, column=column-1).value
+            chap_inc = chap[i] - int(yesterday)
+            if chap_inc > 0:
+                ws.cell(row, column, '(+' + str(chap_inc) + ')')
+            column += 2
 
 wb.save(filename)
